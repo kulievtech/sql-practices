a725c8c78bc2e80f4621e8c384f7b806f8ff49df
Tohir Kuliev <kuliev.tech@gmail.com>
2025-02-10 10:40:59 -0500

    Change formatting

--- 1_select-names.sql
+++ 1_select-names.sql
@@ -7,63 +7,71 @@
 -- The % is a wild-card it can match any characters
 -- Find the country that start with Y
 
-SELECT name FROM world
-  WHERE name LIKE 'Y%'
+SELECT name 
+FROM world
+WHERE name LIKE 'Y%'
 
 -- 2: 
 
 -- Find the countries that end with y
 
-SELECT name FROM world
-  WHERE name LIKE '%Y'
+SELECT name 
+FROM world
+WHERE name LIKE '%Y'
 
 -- 3:
 
 -- Luxembourg has an x - so does one other country. List them both.
 -- Find the countries that contain the letter x
 
-SELECT name FROM world
-  WHERE name LIKE '%X%'
+SELECT name 
+FROM world
+WHERE name LIKE '%X%'
 
 -- 4: 
 
 -- Iceland, Switzerland end with land - but are there others?
 -- Find the countries that end with land
 
-SELECT name FROM world
-  WHERE name LIKE '%LAND'
+SELECT name 
+FROM world
+WHERE name LIKE '%LAND'
 
 -- 5: 
 
 -- Columbia starts with a C and ends with ia - there are two more like this.
 -- Find the countries that start with C and end with ia
 
-SELECT name FROM world
-  WHERE name LIKE 'C%IA'
+SELECT name 
+FROM world
+WHERE name LIKE 'C%IA'
 
 -- 6: 
 
 -- Greece has a double e - who has a double o?
 -- Find the country that has oo in the name
 
-SELECT name FROM world
-  WHERE name LIKE '%oo%'
+SELECT name 
+FROM world
+WHERE name LIKE '%oo%'
 
 -- 7: 
 
 -- Bahamas has three a - who else?
 -- Find the countries that have three or more a in the name
 
-SELECT name FROM world
-  WHERE name LIKE '%a%a%a%'
+SELECT name 
+FROM world
+WHERE name LIKE '%a%a%a%'
 
 -- 8: 
 
 -- India and Angola have an n as the second character. You can use the underscore as a single character wildcard.
 -- Find the countries that have "t" as the second character.
 
-SELECT name FROM world
- WHERE name LIKE '_t%'
+SELECT name 
+FROM world
+WHERE name LIKE '_t%'
 ORDER BY name
 
 -- 9: 
@@ -71,16 +79,18 @@ ORDER BY name
 -- Lesotho and Moldova both have two o characters separated by two other characters.
 -- Find the countries that have two "o" characters separated by two others.
 
-SELECT name FROM world
- WHERE name LIKE '%o__o%'
+SELECT name 
+FROM world
+WHERE name LIKE '%o__o%'
 
 -- 10: 
 
 -- Cuba and Togo have four characters names.
 -- Find the countries that have exactly four characters.
 
-SELECT name FROM world
- WHERE name LIKE '____'
+SELECT name 
+FROM world
+WHERE name LIKE '____'
 
 
 
@@ -93,8 +103,8 @@ SELECT name FROM world
 -- Find the country where the name is the capital city.
 
 SELECT name
-  FROM world
- WHERE name = capital
+FROM world
+WHERE name = capital
 
 
 -- 12: 
@@ -107,26 +117,26 @@ SELECT name
 -- The function concat is short for concatenate - you can use it to combine two or more strings.
 
 SELECT name
-  FROM world
- WHERE capital = concat (name, ' City')
+FROM world
+WHERE capital = concat (name, ' City')
 
 
  -- 13: 
 
  -- Find the capital and the name where the capital includes the name of the country.
 
- Select capital, name
-  From world
- Where capital LIKE concat ('%', name, '%');
+Select capital, name
+From world
+Where capital LIKE concat ('%', name, '%');
 
  -- 14 :
 
  -- Find the capital and the name where the capital is an extension of name of the country.
  -- You should include Mexico City as it is longer than Mexico. You should not include Luxembourg as the capital is the same as the country.
 
- Select capital, name
-  From world
- Where capital LIKE concat (name, ' %');
+Select capital, name
+From world
+Where capital LIKE concat (name, ' %');
 
  -- 15:
 
@@ -139,12 +149,10 @@ SELECT name
 
 -- In this example you remove all the 'a's from the name of each country. This happens because the string 'a' is replaced with .
 
-SELECT name,
-       REPLACE(name, 'a','')
-  FROM bbc
+SELECT name, REPLACE(name, 'a','')
+FROM bbc
 
 
-SELECT name,
-    REPLACE (capital, name, '') AS capital_extension
-  FROM world
- WHERE capital LIKE concat (name, '_%')
+SELECT name, REPLACE (capital, name, '') AS capital_extension
+FROM world
+WHERE capital LIKE concat (name, '_%')
