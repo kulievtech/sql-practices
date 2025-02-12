e5fad30f7ffb4b6693a095bd3defddbc1270ff76
Tohir Kuliev <kuliev.tech@gmail.com>
2025-02-12 11:15:07 -0500

    Finish up to exercise 8

--- sql-zoo/8_using-null.sql
+++ sql-zoo/8_using-null.sql
@@ -72,3 +72,57 @@ SELECT t.name, d.name
 FROM teacher t
 RIGHT JOIN dept d
 ON t.dept = d.id;
+
+
+-- 5:
+
+-- Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266'
+
+
+SELECT name, COALESCE(mobile, '07986 444 2266')
+FROM teacher;
+
+
+
+
+
+-- 6: 
+
+-- Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
+
+
+SELECT t.name, COALESCE(d.name, "None")
+FROM teacher t
+LEFT JOIN dept d
+ON t.dept = d.id;
+
+
+
+
+
+-- 7: 
+
+
+-- Use COUNT to show the number of teachers and the number of mobile phones.
+
+
+
+SELECT COUNT(id), COUNT(mobile)
+FROM teacher;
+
+
+
+-- 8:
+
+
+
+-- Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
+
+
+
+
+SELECT d.name, count(t.id)
+FROM teacher t
+RIGHT JOIN dept d
+ON t.dept = d.id
+GROUP BY d.name;
